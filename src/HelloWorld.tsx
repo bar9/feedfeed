86456e45d4b55ec1c61c23e4483d2a9b6e89fad6
rb <r.brand@iwf.ch>
2022-06-03 16:26:50 +0200

    simple react / antd / esbuild setup

---
+++ src/HelloWorld.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { DatePicker} from "antd";
+
+const HelloWorld = () => {
+    return (
+        <>
+            <h3>Hello World</h3>
+            <DatePicker></DatePicker>
+        </>
+    );
+};
+
+export default HelloWorld;
